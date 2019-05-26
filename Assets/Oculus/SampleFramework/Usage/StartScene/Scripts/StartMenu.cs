058aabbbf8b2b7d2170ed413f750fd5365ed2992
Mario G. Nava García <woodynava93@gmail.com>
2019-05-26 17:27:05 -0500

    Se agregaron divesos modelos 3D para agregar variedad al consultorio. Se eliminaron un conjunto de assets inecesarios incluídos en los ejemplos de Unity Se crearon las 3 escenas principales: -MainMenu -Lerning -Test Junto con otra escena de prueba para mejorar el rendimiento en VR

--- Assets/Oculus/SampleFramework/Usage/StartScene/Scripts/StartMenu.cs
+++ Assets/Oculus/SampleFramework/Usage/StartScene/Scripts/StartMenu.cs
@@ -12,9 +12,10 @@ public class StartMenu : MonoBehaviour
 
     void Start()
     {
-        DebugUIBuilder.instance.AddLabel("Select Sample Scene");
-        DebugUIBuilder.instance.AddButton("Avatar Grab", LoadAvatarGrab);
-        DebugUIBuilder.instance.AddButton("Custom Controllers", LoadCustomControllers);
+        DebugUIBuilder.instance.AddLabel("Selecciona una modalidad");
+        DebugUIBuilder.instance.AddButton("Aprendizaje libre", LoadAprendizaje);
+        DebugUIBuilder.instance.AddButton("Prueba de aprendizaje", LoadPrueba);
+        /*
         DebugUIBuilder.instance.AddButton("Custom Hands", LoadCustomHands);
         DebugUIBuilder.instance.AddButton("Debug UI", LoadDebugUI);
         DebugUIBuilder.instance.AddButton("Distance Grab", LoadDistanceGrab);
@@ -22,6 +23,8 @@ public class StartMenu : MonoBehaviour
         DebugUIBuilder.instance.AddButton("Locomotion", LoadLocomotion);
         DebugUIBuilder.instance.AddButton("Mixed Reality Capture", LoadMixedRealityCapture);
         DebugUIBuilder.instance.AddButton("OVR Overlay", LoadOVROverlay);
+         */
+        
         DebugUIBuilder.instance.Show();
     }
 
@@ -49,13 +52,13 @@ public class StartMenu : MonoBehaviour
     }
 
     #region Scene Load Callbacks
-    void LoadAvatarGrab()
+    void LoadAprendizaje()
     {
-        LoadScene("AvatarGrab");
+        LoadScene("LerningScene");
     }
-    void LoadCustomControllers()
+    void LoadPrueba()
     {
-        LoadScene("CustomControllers");
+        LoadScene("TestScene");
     }
     void LoadCustomHands()
     {
