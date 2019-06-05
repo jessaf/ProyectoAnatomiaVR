4ec082c29bd69a36ba378109ddd307a89381ae7b
Mario G. Nava García <woodynava93@gmail.com>
2019-06-05 04:47:43 -0500

    Se agregó audio ambiental a la escena principal Botón de salida en la puerta de la escena principal Se corrigieron algunos bugs y posición de objetos

---
+++ Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void LoadScene(int sceneANumber)
+    {
+        Debug.Log("sceneBuildIndex to load: " + sceneANumber);
+        SceneManager.LoadScene(sceneANumber);
+    }
+}
